9f193d78118ea32479b2aa2915abf06e8c06b540
Makley <tibolamakley1@gmail.com>
2024-03-31 23:38:36 -0300

    chore: format the files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-	fileServerFolder: 'dist',
+	fileServerFolder: "dist",
 	fixturesFolder: false,
-	projectId: 'etow1b',
+	projectId: "etow1b",
 	e2e: {
-		baseUrl: 'http://localhost:4173/',
-		specPattern: 'cypress/e2e/**/*.ts'
-	}
-})
+		baseUrl: "http://localhost:4173/",
+		specPattern: "cypress/e2e/**/*.ts",
+	},
+});

a96b1c7458adcec339d41f2b39d7ef339e3d3511
Makley <tibolamakley1@gmail.com>
2024-04-02 03:35:52 -0300

    test: add error to test sentry

--- src/pages/login/login.view.tsx
+++ src/pages/login/login.view.tsx
@@ -9,7 +9,13 @@ export const LoginView = withAuthInPublicRoute(() => {
 		<div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
 			<img src={SpotifyLogo} alt="Spotify logo" />
 			<span className="text-center font-bold">Entre com a sua conta Spotify clicando no botão abaixo</span>
-			<Button onClick={SpotifyAuthService.getUserAuthorization}>Entrar</Button>
+			<Button
+				onClick={() => {
+					throw new Error("puts");
+				}}
+			>
+				Entrar
+			</Button>
 		</div>
 	);
 });
